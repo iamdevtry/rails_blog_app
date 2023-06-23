cc836fdebfe549a41923fd4ad12325a971ea4f74
iamdevtry <tranbinhphongcmg@gmail.com>
2023-06-23 23:59:44 +0700

    modified name of controller and view
    modified name of controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Defines the blog posts path route
+  get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "blog_posts#index"
 end
