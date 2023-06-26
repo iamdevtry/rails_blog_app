7248eb9994d296cbcef9ad37545b3160d0938e99
iamdevtry <tranbinhphongcmg@gmail.com>
2023-06-26 22:46:50 +0700

    add authentication

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class BlogPostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
-  # before_action :set_blog_post, except: [:index, :create, :new]
+  # before_action :set_blog_post, except: [:index, :create, :new
+
+
   def index
     @blog_posts = BlogPost.all
   end
